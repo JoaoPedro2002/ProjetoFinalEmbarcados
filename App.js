9e49600c0b10d0ec5cdb8b4dd9a75909352c0bde
João Pedro Cardoso Barbosa <joao.c.barbosa2002@gmail.com>
2022-03-22 20:17:47 -0300

    Métodos chamam requests para o banco

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
+import MainWindow from './components/MainWindow';
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
-
-// or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get
-        a shareable url.
-      </Text>
       <Card>
-        <AssetExample />
+        <MainWindow />
       </Card>
     </View>
   );
@@ -29,11 +22,5 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+  }
 });
---
+++ components/MainWindow.js
@@ -0,0 +1,101 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Slider } from 'react-native-elements';
+import { Switch } from 'react-native-paper';
+
+export default function AssetExample() {
+  const [red, setRed] = React.useState(0);
+  const [green, setGreen] = React.useState(0);
+  const [blue, setBlue] = React.useState(0);
+  const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+  const [isSwitchOnAuto, setIsSwitchOnAuto] = React.useState(false);
+  const [conectado, setConectado] = React.useState(true);
+
+  const ligar = function () {
+    setIsSwitchOn(!isSwitchOn);
+    request({
+      ligado: !isSwitchOn,
+    });
+  };
+
+  const automatico = function () {
+    setIsSwitchOnAuto(!isSwitchOnAuto);
+    request({
+      automatico: !isSwitchOnAuto,
+    });
+  };
+
+  const changeGreen = function (value) {
+    setGreen(value);
+    request({
+      g: value,
+    });
+  };
+
+  const changeRed = function (value) {
+    setRed(value);
+    request({
+      r: value,
+    });
+  };
+
+  const changeBlue = function (value) {
+    setBlue(value);
+    request({
+      b: value
+    });
+  };
+
+  const request = function (body) {
+    const requestOptions = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body),
+    };
+    fetch('http://localhost:3000/estado/0', requestOptions)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+        setConectado(false);
+      });
+  };
+
+  const styles = StyleSheet.create({
+    square: {
+      width: 100,
+      height: 100,
+      background: `rgb(${red}, ${green}, ${blue})`,
+    },
+  });
+
+  return (
+    <View>
+      <Text>{conectado ? '' : 'Conecte-se a um dispositivo'}</Text>
+      {isSwitchOnAuto ? 'Automatico' : 'Manual'}
+      <Switch value={isSwitchOnAuto} onValueChange={automatico} />
+      {isSwitchOn ? 'Ligado' : 'Desligado'}
+      <Switch value={isSwitchOn} onValueChange={ligar} />
+      <View style={styles.square} />
+      <Text> Vermelho: {Math.floor(red)} </Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={255}
+        onValueChange={(value) => {
+          changeRed(value);
+        }}></Slider>
+      <Text> Verde: {Math.floor(green)} </Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={255}
+        onValueChange={(value) => changeGreen(value)}></Slider>
+      <Text> Azul: {Math.floor(blue)} </Text>
+      <Slider
+        minimumValue={0}
+        maximumValue={255}
+        onValueChange={(value) => changeBlue(value)}></Slider>
+    </View>
+  );
+}
